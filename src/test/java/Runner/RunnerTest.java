ea78677024db700e42135b22ed3b418339a4f364
Ajit kumar <59956826+Ajit111@users.noreply.github.com>
2025-09-19 15:27:29 +0530

    Update RunnerTest.java

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -6,8 +6,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/Features", tags="@Test",glue = {"StepDefinition","base"},
-plugin = {"pretty", "html:target/HTML-reports/test.html","json:target/cucumber-report/cucumber.json"})
+// @CucumberOptions(features="src/test/java/Features", tags="@Test",glue = {"StepDefinition","base"},
+// plugin = {"pretty", "html:target/HTML-reports/test.html","json:target/cucumber-report/cucumber.json"})
+  @CucumberOptions(
+    plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"},
+    features = "src/test/resources/features",
+    glue = {"stepDefinitions"}
+)
 
 
 public class RunnerTest {
